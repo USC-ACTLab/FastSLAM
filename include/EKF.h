6be11823c099ff0b96447f5a1d7bf229930096b2
Will Wu <williamwu8-8@outlook.com>
2024-06-03 20:48:45 -0400

    Wwu/alp 62 implement landmark ekf (#17)
    * ALP-62: Add RobotManager and EKF
    
    * ALP-62: Add RobotManager and EKF
    
    * ALP-62: Refactor RobotManager Function signatures
    
    * ALP-62: Finish EKF classes and supporting code
    
    - specify RobotManager and draft functions
    - mock RobotManager needed for testing, see ALP-109
    
    ALP-62: Add robot manager and start EKF dev
    
    ALP-62: Add Unit Tests to EKF class
    
    ALP-62: Add enum for results and pointer deletion
    
    ALP-62: Resolve robot-manager reference
    
    and implement set-up/tear-down of non-empty KF unit test
    
    ALP-62L implement measJacobian() function
    
    ALP-62: Finalized Kalman Filter
    
    * ALP-62: Correct unit test for shared pointer

---
+++ include/EKF.h
@@ -0,0 +1,145 @@
+/**
+ * @file EKF.h
+ * @brief Defines the EKF class
+ */
+
+#pragma once
+
+#include "core-structs.h"
+#include "math-util.h"
+#include "robot-manager.h"
+#include "Eigen/Dense"
+#include <memory>
+
+enum class KF_RET { SUCCESS = 0, EMPTY_ROBOT_MANAGER = -1, MATRIX_INVERSION_ERROR = -2 };
+
+/**
+ * @brief: Abstract EKF class that offers two core EKF functions
+ */
+class EKFBase {
+
+public:
+
+   /**
+    * @brief updates internal state and covariance based on motion model
+    * @details this function calculates the internal beliefs of the state and covariance
+    */
+   virtual KF_RET predict() = 0;
+
+   /**
+    * @brief corrects internal state and covariane based on measurement
+    * @details this function updates both the mean and the covariance based on the filter gain
+    */
+   virtual KF_RET update() = 0;
+
+};
+
+class LandMarkEKF : public EKFBase {
+
+protected:
+
+public:
+
+   /**
+    * @brief calculate measurement's likelihood of correspondence to the landmark
+    * @details this function utilizes Maximum Likelihood Estimation (MLE) and is
+    * used to solve the data association problem
+    */
+   virtual float calcCPD() = 0;
+
+   /**
+    * @brief virtual desturctor, necessary for accessing derived class through base class pointer
+    */
+   virtual ~LandMarkEKF() { };
+
+};
+
+class LMEKF2D final: public LandMarkEKF {
+
+private:
+
+   /**
+    * @brief 2D landmark coordinate estimate
+    */
+   struct Point2D m_mu;
+
+   /**
+    * @brief landmark estimate covariance matrix
+    */
+   Eigen::Matrix2f m_sigma;
+
+   /**
+    * @brief shared pointer to the robot manager instance, used to access measurement models
+    */
+   std::shared_ptr<RobotManager2D> m_robot;
+
+   /**
+    * @brief measurement covariance matrix, used for correspondence weight and K-gain calculation
+    */
+   Eigen::Matrix2f m_meas_cov;
+
+   /**
+    * @brief measurement jacobian helper function, takes the Jacobian of g(s, mu)
+    */
+   Eigen::Matrix2f measJacobian() const;
+
+   /**
+    * @brief function to calculate Kalman Filter gain for measurement correction
+    *
+    * @return A 2x2 matrix of Kalman Filter gain
+    */
+   Eigen::Matrix2f calcKalmanGain() const;
+
+   /**
+    * @brief calculate measurement covariance, store result in member variable
+    */
+   void calcMeasCov();
+
+public:
+
+   /**
+    * @brief default constructor, initialize mu and sigma to 0, robot to nullptr
+    */
+   LMEKF2D();
+
+   /**
+    * @brief parametrized constructor
+    *
+    * @param[in] init_obs: landmark starting location (initial observation)
+    * @param[in] init_cov: P_0, aka initial covariance/uncertainty
+    * @param[in] robot_ptr: shared pointer to the robot manager instance
+    */
+   LMEKF2D(struct Point2D init_obs, Eigen::Matrix2f init_cov,
+           std::shared_ptr<RobotManager2D> robot_ptr);
+
+   /**
+    * @brief default destructor
+    */
+   ~LMEKF2D();
+
+   /**
+    * @brief advances internal beliefs of 2D Landmark using the prediction model
+    * @details no-op in Landmark EKF, since target is non dynamic
+    */
+   KF_RET predict() override {
+      return KF_RET::SUCCESS;
+   }
+
+   /**
+    * @brief collects new measurement and update internal beliefs based on measurement
+    */
+   KF_RET update() override;
+
+   /**
+    * @brief calculates likelihood of correspondence given the measurement and prediction
+    * @return float; scalar value measuring likelihood that the observation matches internal state
+    */
+   float calcCPD() override;
+
+   /**
+    * @brief returns current landmark position estimate in world frame
+    * @return a 2D point struct with landmark x and y position
+    * */
+   struct Point2D getLMEst() const;
+
+};
---
+++ include/robot-manager.h
@@ -0,0 +1,67 @@
+/**
+ * @file robot-manager.h
+ * @brief Defines RobotManager and its derived classes for robot-specific code
+ */
+
+#pragma once
+
+#include "core-structs.h"
+#include "math-util.h"
+#include "Eigen/Dense"
+#include <iostream>
+
+class RobotManager {
+public:
+    virtual void sampleIMU() = 0;
+    virtual void sampleLandMark() = 0;
+    virtual void sampleControl() = 0;
+};
+
+class RobotManager2D : public RobotManager {
+
+protected:
+    struct Pose2D m_curr_pose;
+    struct VelocityCommand2D m_curr_command;
+    const Eigen::Matrix2f m_meas_noise;
+    struct Observation2D m_curr_obs;
+
+    RobotManager2D(struct Pose2D init_pose,
+                   struct VelocityCommand2D init_cmd,
+                   Eigen::Matrix2f meas_noise) : m_curr_pose(init_pose),
+        m_curr_command(init_cmd), m_meas_noise(meas_noise){
+        m_curr_obs = { .range_m = 0, .bearing_rad = 0 };
+    }
+    RobotManager2D() {
+        m_curr_pose = {.x = 0, .y = 0, .theta_rad = 0};
+        m_curr_command = {.vx_mps = 0, .wz_radps = 0};
+    }
+    virtual ~RobotManager2D() {};
+
+public:
+    virtual void motionUpdate() = 0;
+    virtual struct Observation2D predictMeas() = 0;
+    virtual struct Observation2D getCurrObs() const = 0;
+    virtual Eigen::Matrix2f measJacobian(const struct Point2D mu_prev) const = 0;
+    virtual Eigen::Matrix2f getMeasNoise() const = 0;
+};
+
+class Create3Manager final : public RobotManager2D {
+
+public:
+    Create3Manager();
+    Create3Manager(struct Pose2D init_pose,
+                   struct VelocityCommand2D init_cmd,
+                   Eigen::Matrix2f meas_noise): RobotManager2D(init_pose, init_cmd, meas_noise) {}
+    ~Create3Manager() {};
+
+
+    void sampleIMU() override;
+    void sampleLandMark() override;
+    void sampleControl() override;
+    void motionUpdate() override;
+    struct Observation2D getCurrObs() const override;
+    struct Observation2D predictMeas() override;
+    Eigen::Matrix2f measJacobian(const struct Point2D mu_prev) const override;
+    Eigen::Matrix2f getMeasNoise() const override;
+
+};
