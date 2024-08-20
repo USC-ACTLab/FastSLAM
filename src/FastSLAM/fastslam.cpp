7cf96b3005f6e34372db0e456366a94d0d300511
Will Wu <williamwu8-8@outlook.com>
2024-08-19 21:44:41 -0400

    Add mock driver code for testing

---
+++ src/FastSLAM/fastslam.cpp
@@ -0,0 +1,32 @@
+#include "particle-filter.h"
+#include "robot-manager.h"
+#include <stdio.h>
+#include <memory.h>
+
+// constants for create3 pose sensor noise
+static const float x_accel_var = 0.0001543f;
+static const float y_accel_var = 0.0001636f;
+static const float theta_var = 0.0000125f;
+
+static const struct Observation2D obs = {.range_m = 0.623143, 
+                                        .bearing_rad = 0.987187};
+
+int main(){
+
+    const struct Pose2D init_pose = {.x = 0, .y = 0, .theta_rad = 0};
+    const struct VelocityCommand2D init_cmd {.vx_mps = 0, .wz_radps = 0};
+    Eigen::Matrix3f rob_process_noise;
+    rob_process_noise.diagonal() << x_accel_var, y_accel_var, theta_var;
+    std::shared_ptr<Create3Manager> m_robot_manager = std::make_shared<Create3Manager>(init_pose, init_cmd, 
+        Eigen::Matrix2f::Zero(), 3.0f, rob_process_noise);
+    std::unique_ptr<FastSLAMPF> m_fastslam_filter = std::make_unique<FastSLAMPF>(
+        std::static_pointer_cast<RobotManager2D>(m_robot_manager));
+
+    std::queue<Observation2D> lidar_landmarks;
+    lidar_landmarks.push(obs);
+
+    m_fastslam_filter->updateFilter(init_pose, lidar_landmarks);
+
+
+    return 0;
+}
